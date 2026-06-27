3b6dd7f299b92201e79898ac9cd99636d4d2f980
JidnyasaBhagat0525 <jidnyasabhagat7@gmail.com>
2026-06-27 16:32:58 +0530

    Implement palindrome number checker
    Fix logic errors and correct variable initialization in palindrome check.

---
+++ palindrome.py
@@ -0,0 +1,14 @@
+# program to check palindrome number
+
+num = int(input("Enter a number: "))
+temp = num
+rev = o
+ while num > 0:
+   digit = num % 10
+   rev = rev * 10 + digit
+   num = num / 10
+
+if num == rev:
+  print("Palindrome Number")
+else:
+  print("Not a Palindrome Number")
