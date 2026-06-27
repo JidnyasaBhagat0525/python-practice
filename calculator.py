27adea954701a3458de807a7defdaccb9eb914b8
JidnyasaBhagat0525 <jidnyasabhagat7@gmail.com>
2026-06-27 10:56:42 +0530

    Add comment to calculator.py

--- calculator.py
+++ calculator.py
@@ -1,7 +1,8 @@
+# simple calculator program using python
 a = 10
 b = 20
 
 print("Addition =", a + b)
-print("Subtraction =", a + b)
-print("Multiplication =", a + b)
-print("Division =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
