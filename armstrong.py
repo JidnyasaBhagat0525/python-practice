6164ad5161a6c20c97181984156302c4878e486c
JidnyasaBhagat0525 <jidnyasabhagat7@gmail.com>
2026-06-27 16:46:41 +0530

    Add Armstrong number checker program

---
+++ armstrong.py
@@ -0,0 +1,16 @@
+# program to check armstrong number
+
+num = int(input("Enter a number: "))
+
+temp = num
+sum = 0
+
+while num > 0:
+  digit = num % 10
+  sum = sum + (digit ** 3)
+  num = num // 10
+
+if temp == sum:
+  print("Aemstrong Number")
+else:
+  print(:Not an Armstrong Number") 
