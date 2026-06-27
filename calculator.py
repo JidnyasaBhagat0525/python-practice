e05bd344730c8282442c0ff431d7f55395dcbe01
JidnyasaBhagat0525 <jidnyasabhagat7@gmail.com>
2026-06-27 11:00:19 +0530

    improve calculator using user input

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 # simple calculator program using python
-a = 10
-b = 20
+a = int(input("Enter first number: "))
+b = int(input("Enter sedond number: "))
 
 print("Addition =", a + b)
 print("Subtraction =", a - b)
